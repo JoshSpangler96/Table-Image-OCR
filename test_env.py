5694cfb716ef576d4c2cfad178dc9428afd4b8ea
Joshua Spangler <jdspangler96@gmail.com>
2021-08-31 11:11:59 -0700

    Update test_env.py

--- test_env.py
+++ test_env.py
@@ -1,12 +1,13 @@
-from read_chart_ocr import ReadChartImage
+from table_img_ocr import ReadTableImage
 
 #image path
 filename = 'test_3.png'
 
-img = ReadChartImage(image_path=filename, tesseract_path=(r'C:\Users\JSPANGLER\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'), show=False)
+img = ReadTableImage(image_path=filename, tesseract_path=(r'C:\Users\JSPANGLER\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'), show=False)
 img.auto_resize_image()
 img.pre_process_image()
 img.create_merge_lines(vert_tolerance=100, horizontal_tolerance=50, overlap_tolerance=100)
 chart_data = img.read_chart()
+img.create_csv()
 for line in chart_data:
     print(line)
